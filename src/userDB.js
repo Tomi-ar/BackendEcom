5acb887bb8ea4e668e6cacb5b1cda9a743521184
Tomi-ar <tomarrillaga@gmail.com>
2022-02-08 19:10:40 -0300

    Cambios generales Clase32 v1.5

--- src/userDB.js
+++ src/userDB.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/users")
+mongoose.connect("mongodb+srv://Tomas:t4VMECAcMAvPYNN@ecommerceatlas.80zrg.mongodb.net/ecommerceAtlas?retryWrites=true&w=majority")
 mongoose.connection.on("open", () => {
     console.log("base de datos conectada");
 })
 mongoose.connection.on("error", () => {
     console.log("Error al abrir la BD");
 })
+
+
+// const session = require("express-session")
+// const cookieParser = require("cookie-parser")
+// const MongoStore = require("connect-mongo")
+// const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+// app.use(cookieParser())
+// app.use(session({
+//     store: MongoStore.create({
+//         mongoUrl: "mongodb+srv://Tomas:t4VMECAcMAvPYNN@ecommerceatlas.80zrg.mongodb.net/ecommerceAtlas?retryWrites=true&w=majority",
+//         mongoOptions: advancedOptions,
+//         collectionName: "sessions"
+//     }),
+//     secret: "sunrise",
+//     saveUninitialized: true,
+//     resave: true,
+//     cookie: {
+//         maxAge: 100000
+//     }
+// }))
